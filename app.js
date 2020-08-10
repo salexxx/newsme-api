577f4f15caf58da0ab5f5b6e27c92146bd199793
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-08-11 00:06:56 +0700

    вынес в  модуль централизованную обработку ошибок

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const { login, createUser } = require('./controllers/user');
 const auth = require('./middlewares/auth');
 const NotFound = require('./errors/notfound');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { centralizedErr } = require('./errors/centralized');
 
 const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/newsme', {
@@ -66,19 +67,8 @@ app.use(errorLogger);
 
 app.use(errors());
 
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-});
+app.use(centralizedErr);
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
 });

1f797177e30389f6727628c9ef8a5032f3a7d122
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-08-12 22:13:26 +0700

    перенес роут /signin / signup в routes/index

--- app.js
+++ app.js
@@ -3,16 +3,12 @@ const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { celebrate, Joi } = require('celebrate');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const { errors } = require('celebrate');
 const router = require('./routes/index');
-const { login, createUser } = require('./controllers/user');
-const auth = require('./middlewares/auth');
-const NotFound = require('./errors/notfound');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { centralizedErr } = require('./errors/centralized');
 const limiter = require('./constants/limiter');
@@ -32,28 +28,7 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
-app.post('/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(6),
-    }),
-  }),
-  login);
-app.post('/signup',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(6),
-      name: Joi.string().required().min(2),
-    }),
-  }),
-  createUser);
-app.use(auth);
 app.use(router);
-app.use('/*', (req, res, next) => {
-  next(new NotFound('Запрашиваемый ресурс не найден'));
-});
 
 app.use(errorLogger);
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,29 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const userRout = require('./users');
 const articleRout = require('./articles');
 const NotFound = require('../errors/notfound');
+const auth = require('../middlewares/auth');
+const { login, createUser } = require('../controllers/user');
 
+router.post('/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(6),
+    }),
+  }),
+  login);
+router.post('/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(6),
+      name: Joi.string().required().min(2),
+    }),
+  }),
+  createUser);
+router.use(auth);
 router.use('/users', userRout);
 router.use('/articles', articleRout);
 router.use('/*', (req, res, next) => {

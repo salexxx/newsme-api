2af5e3f67de3d4966df7273bc3d1fc89c1d28069
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-08-12 21:53:08 +0700

    перенес роут /* в routes/index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const userRout = require('./users');
 const articleRout = require('./articles');
+const NotFound = require('../errors/notfound');
 
 router.use('/users', userRout);
 router.use('/articles', articleRout);
-
+router.use('/*', (req, res, next) => {
+  next(new NotFound('Запрашиваемый ресурс не найден'));
+});
 module.exports = router;

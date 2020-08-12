b4a3ddfab2ea12ee8fbab1088c54bcf180c8a90f
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-08-12 15:02:22 +0700

    убрал закомментированный код

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,5 @@
 const userRout = require('express').Router();
-// const { celebrate, Joi } = require('celebrate');
 const { getUser } = require('../controllers/user');
-
-// userRout.get('/', getUsers);
 
 userRout.get('/me', getUser);
 module.exports = userRout;

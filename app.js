e58afd5e7ea33eac2531672f02b2b3c81c8cb4e3
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-08-12 23:06:17 +0700

    перенес роут /crash-test в routes/index

--- app.js
+++ app.js
@@ -22,18 +22,9 @@ mongoose.connect(process.env.NODE_ENV === 'production' ? process.env.DB : 'mongo
 app.use(limiter);
 app.use(helmet());
 app.use(requestLogger);
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.use(router);
-
 app.use(errorLogger);
-
 app.use(errors());
-
 app.use(centralizedErr);
 
 app.listen(process.env.PORT || 3000, () => {
